f72081006a9befc4e8f5c47b6f2d85a67344d542
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-07-09 17:35:45 +0200

    new database added

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Progress } from "@/components/ui/progress";
-//import { useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import { useUploadThing } from "@/lib/uploadthing";
 import { cn } from "@/lib/utils";
 import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
@@ -10,36 +10,37 @@ import { useState, useTransition } from "react";
 import Dropzone, { FileRejection } from "react-dropzone";
 
 const Page = () => {
-  //const { toast } = useToast();
+  const { toast } = useToast();
   const [isDragOver, setIsDragOver] = useState<boolean>(false);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
   const router = useRouter();
-    const { startUpload, isUploading } = useUploadThing("imageUploader", {
-      onClientUploadComplete: ([data]) => {
-        const configId = data.serverData.configId;
-        startTransition(() => {
-          router.push(`/configure/design?id=${configId}`);
-        });
-      },
-      onUploadProgress(p) {
-        setUploadProgress(p);
-      },
-    });
+
+  const { startUpload, isUploading } = useUploadThing("imageUploader", {
+    onClientUploadComplete: ([data]) => {
+      const configId = data.serverData.configId;
+      startTransition(() => {
+        router.push(`/configure/design?id=${configId}`);
+      });
+    },
+    onUploadProgress(p) {
+      setUploadProgress(p);
+    },
+  });
 
   const onDropRejected = (rejectedFiles: FileRejection[]) => {
     const [file] = rejectedFiles;
 
     setIsDragOver(false);
 
-    // toast({
-    //   title: `${file.file.type} type is not supported.`,
-    //   description: "Please choose a PNG, JPG, or JPEG image instead.",
-    //   variant: "destructive",
-    // });
+    toast({
+      title: `${file.file.type} type is not supported.`,
+      description: "Please choose a PNG, JPG, or JPEG image instead.",
+      variant: "destructive",
+    });
   };
 
   const onDropAccepted = (acceptedFiles: File[]) => {
-    //startUpload(acceptedFiles, { configId: undefined });
+    startUpload(acceptedFiles, { configId: undefined });
 
     setIsDragOver(false);
   };
@@ -50,7 +51,9 @@ const Page = () => {
     <div
       className={cn(
         "relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",
-        { "ring-blue-900/25 bg-blue-900/10": isDragOver }
+        {
+          "ring-blue-900/25 bg-blue-900/10": isDragOver,
+        }
       )}
     >
       <div className="relative flex flex-1 flex-col items-center justify-center w-full">
--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,4 +1,4 @@
-import { OurFileRouter } from "@/app/api/auth/[kindeAuth]/uploadthing/core";
+import { OurFileRouter } from "@/app/api/uploadthing/core";
 import { generateReactHelpers } from "@uploadthing/react";
 
 export const { useUploadThing, uploadFiles } =
