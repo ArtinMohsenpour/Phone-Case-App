bae645278e8242419dc382fcbeec5cda1556e452
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-07-23 16:29:02 +0200

    update

--- src/app/auth-callback/actions.ts
+++ src/app/auth-callback/actions.ts
@@ -1,19 +1,19 @@
-"use server";
+'use server'
 
-import { db } from "@/db";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const getAuthStatus = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
   if (!user?.id || !user.email) {
-    throw new Error("Invalid user data");
+    throw new Error('Invalid user data')
   }
 
   const existingUser = await db.user.findFirst({
     where: { id: user.id },
-  });
+  })
 
   if (!existingUser) {
     await db.user.create({
@@ -21,8 +21,8 @@ export const getAuthStatus = async () => {
         id: user.id,
         email: user.email,
       },
-    });
+    })
   }
 
-  return { success: true };
-};
+  return { success: true }
+}
--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -19,7 +19,7 @@ const Page = () => {
     queryKey: ["auth-callback"],
     queryFn: async () => await getAuthStatus(),
     retry: true,
-    retryDelay: 5000,
+    retryDelay: 500,
   });
 
   if (data?.success) {
